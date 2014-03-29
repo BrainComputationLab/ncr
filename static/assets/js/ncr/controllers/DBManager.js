6bb22e07ab5efa6994132fb9ed4a2e1fa261d08d
Alex <exavior75@yahoo.com>
2014-03-29 14:19:06 -0700

    Delete DBManager.js

--- static/assets/js/ncr/controllers/DBManager.js
+++
@@ -1,4 +0,0 @@
-﻿//Creates a new Angular module for our UserManager application.
-var DBManager = angular.module('DBManager', ['ngResource']);
-//Register the controller with the module.
-DBManager = DBManager.controller('DBController', DBController);
